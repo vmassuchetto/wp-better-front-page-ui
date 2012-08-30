3c70b92b2a0287302786c965cbcfb7a1c4f821f6
Leo Germani <leogermani@hacklab.com.br>
2012-08-30 10:25:57 -0300

    fully functional, except for menu items. better description

--- force-front-page.php
+++ force-front-page.php
@@ -10,27 +10,34 @@
 
 class Force_Front_Page {
 
+    var $option_name;
+    var $default_value;
+    
     function Force_Front_Page() {
+        $this->option_name = 'force_front_page_posts_page';
+        $this->default_value = 'blog';
+        
         register_activation_hook( __FILE__, array( $this, 'activate' ) );
         if ( is_admin() ) {
             add_action( 'admin_init', array( $this, 'admin_init' ) );
-            #add_filter( 'whitelist_options', array( $this, 'whitelist_options' ) );
         }
         add_action( 'rewrite_rules_array', array( $this, 'rewrite_rules_array' ), 9999 );
         add_filter( 'option_show_on_front', array( $this, 'filter_show_on_front' ) );
         add_filter( 'query_vars', array( $this, 'query_vars' ) );
-        #add_action( 'template_include', array( $this, 'template' ) );
         add_action( 'admin_print_footer_scripts', array( $this, 'remove_reading_option' ) );
     }
-
+    
+    function get_option() {
+        $option = get_option($this->option_name);
+        return !$option || empty($option) ? $this->default_value : $option;
+    }
+    
     function activate() {
-        if ( ! $option = get_option( 'force_front_page_posts_page' ) )
-            update_option( 'force_front_page_posts_page', 'posts' );
+        //TODO: Flush Rules
     }
 
     function rewrite_rules_array( $rules ) {
-        $option = get_option( 'force_front_page_posts_page' );
-        #var_dump($rules); die;
+        $option = $this->get_option();
         $posts_home_rule = array(
             $option . '/?$' => 'index.php?force_home=1',
             $option . '/page/?([0-9]{1,})/?$' => 'index.php?force_home=1&paged=$matches[1]' ,
@@ -43,44 +50,47 @@ class Force_Front_Page {
         return $vars;
     }
 
-    function template( $template ) {
-        $templates = array ( 'front-page.php', 'home.php', 'index.php' );
-        if ( get_query_var( 'posts_home' ) ) {
-            array_shift( $templates );
-            return locate_template( $templates );
-        } elseif ( is_home() ) {
-            return locate_template( $templates );
-        }
-        return $template;
-    }
-    
     // If it existed, we could filter is_front_page, but since it relies on this option, we filter it here
     function filter_show_on_front($value) {
+        
+        // TODO: DO we really need to bother with this? Is this a good thingo to do?
+        // The point here is to filter only when is_front_page() calls it
+        #$callStack = debug_backtrace();
+        #if (!is_array($callStack) || !isset($callStack[4]) || !is_array($callStack[4]) || !isset($callStack[4]['function']) || $callStack[4]['function'] != 'is_front_page')
+        #    return $value;
+        ///////////////////////////////////////////////////////////////////////////////////
+        $value = 'posts';
         if (is_home() && get_query_var('force_home') == 1)
             $value = 'force';
         return $value;
-        
     }
     
     function admin_init() {
         
-        add_settings_field( 'force-front-page-posts-page', __( 'Post Home Page', 'force_front_page' ),
-            array( $this, 'register_setting_reading' ), 'permalink', 'optional' );
+        add_settings_field( $this->option_name, __( 'Post Home Page', 'force_front_page' ),
+            array( $this, 'output_setting_form' ), 'permalink', 'optional' );
             
-        register_setting( 'permalink', 'force-front-page-posts-page', array($this, 'sanitize_option') );
+        // sadly register_setting is useless in the permalink page, so we will have to save it on our own
+        //register_setting( 'permalink', $this->option_name, array($this, 'sanitize_option') );
+        
+        global $pagenow;
+        if ($pagenow == 'options-permalink.php') {
+            if (isset($_POST[$this->option_name])) {
+                $value = $this->sanitize_option($_POST[$this->option_name]);
+                update_option($this->option_name, $value);
+            } else {
+                delete_option($this->option_name);
+            }
+            
+        }
     }
     
     function sanitize_option($value) {
-        return 'a' . $value;
-    }
-    
-    function whitelist_options( $options ) {
-        $options['permalink'][] = 'force_front_page_posts_page';
-        return $options;
+        return sanitize_title($value);
     }
 
-    function register_setting_reading() {
-        $option = get_option( 'force_front_page_posts_page' );
+    function output_setting_form() {
+        $option = $this->get_option();
         ?>
         <p class="force-front-page-posts-page"><label>
             <?php echo home_url(); ?>/<input name="force_front_page_posts_page" type="text" value="<?php echo $option; ?>" class="tog" />
@@ -104,6 +114,8 @@ class Force_Front_Page {
 }
 
 function force_front_page_init() {
-    new Force_Front_Page();
+    //This plugin only makes sense if you have a front_page.php in your theme
+    if (locate_template('front-page.php'))
+        new Force_Front_Page();
 }
-add_action( 'plugins_loaded', 'force_front_page_init' );
+add_action( 'init', 'force_front_page_init' );
--- readme.txt
+++ readme.txt
@@ -7,44 +7,52 @@ Tested up to: 3.4
 Stable tag: 0.01
 License: GPLv2 or later
 
-Force the front page without any user interference or dummy pages. The site
-home page will always load the `front-page.php` template, and you will be asked
-in the reading settings to define what URL will be used to the posts home page
-and fallback to `home.php` template.
+Lets you specify the URL for your blog when you have a front-page.php file in your theme so you dont have to create two dummy pages as placeholders for your Home and your Blog.
+
+This plugin does not touch the Template hierarchy. All it does is setup an URL to access your blog (is_home()) when
+you have a front_page.php file in your theme. It also removes the "Front Page Displays" option from the Reading Settings.
+
+Note: This plugin will only work if you have a front-page.php file in your theme
 
 == Description ==
+
+Lets you specify the URL for your blog when you have a front-page.php file in your theme so you dont have to create two dummy pages as placeholders for your Home and your Blog.
+
+This plugin does not touch the Template hierarchy. All it does is setup an URL to access your blog (is_home()) when
+you have a front_page.php file in your theme. It also removes the "Front Page Displays" option from the Reading Settings.
+
+WordPress should deal with the site home page. The supported way today is:
+
+1. Define a `front-page.php` for the site home and (optionally) a `home.php` for the posts
+home.
+2. Go to the site admin and create two dummy page.
+3. Set this page as the front and posts pages in the reading settings.
+
+This approach is too sensible, and will let users delete this dummy pages,
+possibily breaking the site.
+
+With this plugin, the same procedure will be:
+
+1. Define a `front-page.php` for the site home and (optionally) a `home.php` for the posts
+home.
+2. Set what URL will point to the posts home in the permalink settings (default is to /blog)
+
+Note: This plugin will only work if you have a front-page.php file in your theme
 
 This plugin idea came from a discussion
 [here](http://lists.automattic.com/pipermail/wp-hackers/2012-August/044235.html),
 [here](http://core.trac.wordpress.org/ticket/16379) and
 [here](http://core.trac.wordpress.org/ticket/18705) (so far) about how
-WordPress should deal with the site home page. The supported way today is:
-
-1. Define a `front-page.php` for the site home and a `home.php` for the posts
-home.
-2. Go to the site admin and create a dummy page.
-3. Set this page as the front page in the reading settings.
-
-This approach is too sensible, and will let users delete this dummy home page,
-possibily breaking the site.
-
-With this plugin, the same procedure will be:
-
-1. Define a `front-page.php` for the site home and a `home.php` for the posts
-home.
-2. Set what URL will point to the posts home and fallback in the `home.php`
-template.
 
 == Installation ==
 
-Upload the to your `wp-content/plugins` directory and configure it on `Settings
--> Reading`.
+Upload the to your `wp-content/plugins` directory and optionally go to `Settings
+-> Permalink`.
 
 == TODO ==
 
-* Load correct body classes
-* Flush rules on reading settings save
-* Reset rules to default on plugin deactivation
+* Flush Rules on activation and Reset rules to default on plugin deactivation
+* Adds the possibility to users add links to home and blog to the menu
 
 == Changelog ==
 
